b1deb95bb3f5ec8950cf7fc5d9a2cd1d93bc19ea
sky-min <81374952+sky-min@users.noreply.github.com>
2021-11-04 22:09:46 +0900

    Update graph.py

--- graph.py
+++ graph.py
@@ -5,16 +5,32 @@ def fx(x): #함수
     return x**3 + 2*x**2 - 4*x - 2
 
 def differential(f, x):  #미분 함수
-    h = 1e-4
+    h = 0.0000000001
     return (f(x+h) - f(x-h)) / (2*h)
 
-x = numpy.arange(-5, 5, 0.01)
+def line(f, x):
+    d = differential(f, x)
+    y = f(x) - d*x
+    return lambda t: d*t + y
+
+xx = float(input('접선의 x좌표를 입력해주세요: '))
+lim_x1, lim_x2 = map(float, input('x의 최솟값과 최대값을 입력해주세요(띄어쓰기로 구별): ').split())
+lim_y1, lim_y2 = map(float, input('y의 최솟값과 최대값을 입력해주세요(띄어쓰기로 구별): ').split())
+
+
+graph.axvline(x = 0, color = 'r')
+graph.axhline(y = 0, color = 'r')
+
+x = numpy.arange(lim_x1, lim_x2, 0.01)
 y = fx(x)
 graph.plot(x, y, 'b') #일반함수 그리기
 
 y2 = differential(fx, x)
-graph.plot(x, y2, 'g')
+graph.plot(x, y2, 'g') #미분한 함수 그리기
 
-graph.axvline(x = 0, color = 'r')
-graph.axhline(y = 0, color = 'r')
+tf = line(fx, xx)
+y3 = tf(x)
+graph.plot(x, y3, 'm') #접선 그리기
+
+graph.ylim(lim_y1, lim_y2)
 graph.show() #그래프 보내기
